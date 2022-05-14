e0ac9fa7df12e79aa35a5aed3802c08de1c53e8b
Jeongyeon Lee <52949397+jeongyeonee@users.noreply.github.com>
2022-05-15 08:51:22 +0900

    Create 1-reverse-string.py

---
+++ 6-string-manipulation/jeongyeon/1-reverse-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        
+        return s.reverse()
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+# 305ms
