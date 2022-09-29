0706f75c63748df1cb8ab5b8fe59a9c3ddfc1684
Jeongyeon Lee <52949397+jeongyeonee@users.noreply.github.com>
2022-09-29 09:46:12 +0900

    Update 8-trapping-rain-water.py

--- 7-array/jeongyeon/8-trapping-rain-water.py
+++ 7-array/jeongyeon/8-trapping-rain-water.py
@@ -1,6 +1,11 @@
 '''
 투포인터 사용
 143 ms
+
+가장 높은 곳에서 두 포인터가 만나면 끝
+(두 포인터 비교해서 높은쪽으로 한 칸 전진)
+
+각 포인터가 여태까지 지나왔던 높이 중 max에서 현재 높이를 빼서 volume에 더함.
 '''
 def trap_use_twopointer(height):
     if not height:
