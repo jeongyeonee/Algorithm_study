0311e6e5078b0ffaf78d51f7359f43567164ccb9
Jeongyeon Lee <52949397+jeongyeonee@users.noreply.github.com>
2022-10-18 20:53:48 +0900

    Update 18-odd-even-linked-list.py

--- 8-linked-list/jeongyeon/18-odd-even-linked-list.py
+++ 8-linked-list/jeongyeon/18-odd-even-linked-list.py
@@ -7,9 +7,8 @@ def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
     even_head = head.next
 
     while even and even.next:
-        # 홀, 짝 상태에서 두 칸씩 이동
+        # head에서 홀, 짝 자리를 정해준 후 두 칸씩 이동
         odd.next, even.next = odd.next.next, even.next.next
-        # 왜 현재 포인트를..왜...한 칸...왜..?
         odd, even = odd.next, even.next
 
     odd.next = even_head
